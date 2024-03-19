7b0fdec890f8f3d271797f99a8111dc93b5d0d2b
bm-avatar <bmatusiak@gmail.com>
2024-03-19 11:47:33 -0400

    update

--- LC_JScript/LC_JScriptClass.cs
+++ LC_JScript/LC_JScriptClass.cs
@@ -1,14 +1,45 @@
-﻿using System;
+﻿
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LC_JScript
 {
-    public class LC_JScriptClass
-    {
 
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LC_JScriptClass : BaseUnityPlugin
+    {
+        // Mod Details
+        private const string modGUID = "UnknownMod";
+        private const string modName = "UnknownMod";
+        private const string modVersion = "1.0.0.0";
+        private readonly Harmony harmony = new Harmony(modGUID);
+        public static ManualLogSource mls;
+        private static LC_JScriptClass Instance;
+
+        private string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        void Awake()
+        {
+            Instance = this;
+            mls = BepInEx.Logging.Logger.CreateLogSource("UnknownMod");
+            mls.LogInfo("(UnknownMod): Patching");
+            harmony.PatchAll(typeof(LC_JScriptClass));
+            
+            mls.LogInfo("(UnknownModJS):");
+
+            mls.LogInfo("Loaded! " + assemblyFolder);
+        }
 
     }
+    
 }
